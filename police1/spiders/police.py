2f997cf9a8dfbef3066b415864a9824f5fe2f205
jake <jake@Ubuntu.myguest.virtualbox.org>
2023-11-13 17:43:20 -0700

    Fixed scrolling

--- police1/spiders/police.py
+++ police1/spiders/police.py
@@ -8,18 +8,15 @@ from scrapy_playwright.page import PageMethod
 class PoliceSpider(scrapy.Spider):
     name = "police"
     allowed_domains = ["www.police1.com"]
-
-    # start_urls = ["https://www.police1.com/law-enforcement-directory/search/"]
+    start_urls = ["https://www.police1.com/law-enforcement-directory/search"]
 
     def start_requests(self):
-        url = "https://www.police1.com/law-enforcement-directory/search/"
+        url = "https://www.police1.com/law-enforcement-directory/search"
         yield scrapy.Request(url, meta=dict(
             playwright=True,
             playwright_include_page=True,
             playwright_page_methods=[
-                PageMethod("wait_for_selector", "a.Table-row"),
-                PageMethod("evaluate", "window.scrollBy(0, document.body.scrollHeight)"),
-                PageMethod("wait_for_selector", "a.Table-row:nth-child(26)"),  # 25 per page
+                PageMethod("wait_for_selector", "a.Table-row"), # 25 per page
             ],
             errback=self.errback,
         ))
@@ -27,11 +24,31 @@ class PoliceSpider(scrapy.Spider):
     async def parse(self, response):
         page = response.meta["playwright_page"]
         await page.close()
-        links = response.css('a.Table-row::attr(href)').getall()
-        number = 0
-        for link in links:
-            number += 1
-            yield print(f'Link #{number}: ', link)
+
+        for link in response.css('a.Table-row::attr(href)'):
+            print(link)
+
+
+        next_page_link_selector = response.xpath('//*[@id="department-list"]/div/div[2]/div/a')
+        next_page_link = next_page_link_selector.xpath('@href').get()
+
+
+        if next_page_link is not None:
+            next_page_url = self.start_urls[0] + next_page_link
+            print(f"Next button was pressed!, next url is {next_page_url}")
+            yield scrapy.Request(next_page_url, meta=dict(
+                playwright = True,
+                playwright_include_page = True,
+                playwright_page_methods = [
+                    PageMethod('wait_for_selector', 'a.Table-row'),
+                ],
+                errback=self.errback,
+            ))
+        else:
+            print("Next button was not pressed")
+
+    async def parse_table(self, response):
+        yield print("SUCCESS!!")
 
 
     async def errback(self, failure):
